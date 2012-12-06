65bc5c824f5fde7b4e7e3eea63cc3fad2dc1b161
Jonathan Ku <jonathanku88@gmail.com>
2012-12-06 11:58:43 -0500

    second push

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Energy::Application.routes.draw do
+
+  match '/map' => 'energy#new', :via => :get
+  match '/map' => 'energy#create', :via => :post
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +51,7 @@ Energy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'energy#new'
 
   # See how all your routes lay out with "rake routes"
 
